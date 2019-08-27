eb78f964d642e64e0f982acd885eeba4c21dd062
Ryan Untalan <untalan.ryan@gmail.com>
2019-08-27 16:13:40 -0700

    first commit

---
+++ src/components/VideoItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Grid, Paper, Typography } from "@material-ui/core";
+
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <Grid item xs={12}>
+      <Paper
+        style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+        onClick={() => onVideoSelect(video)}
+      >
+        <img
+          style={{
+            width: "50%",
+            height: "50%",
+            marginRight: "20px"
+          }}
+          alt="thumbnail"
+          src={video.snippet.thumbnails.medium.url}
+        />
+        <Typography variant="subtitle1">
+          <b>{video.snippet.title}</b>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default VideoItem;
